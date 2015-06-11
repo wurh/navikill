d371132a9698bef676c8f93ea3b511dc20577f30
wurh <wurh@users.noreply.github.com>
2015-06-11 11:18:49 +0800

    [UPD] 更新配置

--- components/config/config.js
+++ components/config/config.js
@@ -8,183 +8,183 @@ module.exports = {
     users:[
         {
             name:'郑炜飚',
-            points:'9',
+            points:'11',
             head:'2'
         },
         {
             name:'关智梁',
-            points:'12',
-            head:'3'
+            points:'16',
+            head:'4'
         },
         {
             name:'陈韩杰',
-            points:'6',
-            head:'2'
+            points:'11',
+            head:'4'
         },
         {
             name:'苏晓',
-            points:'4',
+            points:'6',
             head:'1'
         },
         {
             name:'储杰',
-            points:'8',
+            points:'12',
             head:'3'
         },
         {
             name:'杨硕',
-            points:'6',
+            points:'8',
             head:'0'
         },
         {
             name:'李炳津',
-            points:'8',
-            head:'0'
+            points:'13',
+            head:'2'
         },
         {
             name:'郭嘉林',
-            points:'7',
-            head:'2'
+            points:'12',
+            head:'3'
         },
         {
             name:'何妮',
-            points:'6',
-            head:'0'
+            points:'9',
+            head:'1'
         },
         {
             name:'谢玄亮',
-            points:'8',
-            head:'2'
-        },
-        {
-            name:'Don',
-            points:'11',
+            points:'15',
             head:'4'
         },
         {
+            name:'Don',
+            points:'15',
+            head:'6'
+        },
+        {
             name:'吴诗伟',
-            points:'6',
-            head:'0'
+            points:'10',
+            head:'3'
         },
         {
             name:'罗芬',
-            points:'8',
+            points:'12',
             head:'0'
         },
         {
             name:'姜淦',
-            points:'2',
-            head:'0'
+            points:'10',
+            head:'2'
         },
         {
             name:'张冬萌',
-            points:'6',
-            head:'1'
+            points:'11',
+            head:'4'
         },
         {
             name:'陈育文',
-            points:'8',
-            head:'1'
+            points:'13',
+            head:'5'
         },
         {
             name:'周波',
-            points:'6',
-            head:'1'
+            points:'9',
+            head:'3'
         },
         {
             name:'林子杰',
-            points:'8',
-            head:'0'
+            points:'13',
+            head:'2'
         },
         {
             name:'胡慧贤',
-            points:'8',
-            head:'1'
+            points:'13',
+            head:'4'
         },
         {
             name:'施中柱',
-            points:'11',
+            points:'13',
             head:'2'
         },
         {
             name:'李景超',
-            points:'12',
-            head:'6'
+            points:'20',
+            head:'10'
         },
         {
             name:'吴岳',
-            points:'8',
-            head:'3'
+            points:'12',
+            head:'5'
         },
         {
             name:'吴荣恩',
-            points:'8',
+            points:'13',
             head:'0'
         },
         {
             name:'陈旭龙',
-            points:'4',
+            points:'6',
             head:'0'
         },
         {
             name:'子文',
-            points:'10',
+            points:'14',
             head:'2'
         },
         {
             name:'陈南蓓',
-            points:'9',
-            head:'1'
+            points:'11',
+            head:'3'
         },
         {
             name:'何炜华',
-            points:'9',
+            points:'14',
             head:'3'
         },
         {
             name:'李梦宜',
-            points:'14',
-            head:'4'
+            points:'19',
+            head:'5'
         },
         {
             name:'谢瑛',
-            points:'10',
-            head:'3'
+            points:'14',
+            head:'5'
         },
         {
             name:'周慧能',
-            points:'6',
+            points:'11',
             head:'1'
         },
         {
             name:'吴荣华',
-            points:'13',
+            points:'17',
             head:'7'
         },
         {
             name:'买买提',
-            points:'9',
+            points:'14',
             head:'3'
         },
         {
             name:'曾帆',
-            points:'9',
-            head:'1'
+            points:'14',
+            head:'4'
         },
         {
             name:'李宇钧',
-            points:'6',
-            head:'1'
+            points:'11',
+            head:'5'
         },
         {
             name:'钱汝锋',
-            points:'6',
-            head:'0'
+            points:'12',
+            head:'3'
         },
         {
             name:'刘永健',
-            points:'9',
-            head:'1'
+            points:'11',
+            head:'2'
         },
         {
             name:'刘洋（3周）',
@@ -193,18 +193,18 @@ module.exports = {
         },
         {
             name:'谢维邦',
-            points:'7',
-            head:'6'
+            points:'9',
+            head:'7'
         },
         {
             name:'易晓键',
-            points:'11',
-            head:'4'
+            points:'16',
+            head:'7'
         },
         {
             name:'陈欣欣',
-            points:'8',
-            head:'0'
+            points:'16',
+            head:'4'
         }
     ]
 }
