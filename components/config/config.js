0e602d907bbe0a608b1eb071b174d84baead2c39
wurh <wurh@users.noreply.github.com>
2015-07-07 14:17:52 +0800

    [UPD] 更新排名

--- components/config/config.js
+++ components/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     users:[
         {
             name:'李景超',
-            points:'2',
+            points:'4',
             head:'0'
         },
         {
@@ -18,63 +18,63 @@ module.exports = {
         },
         {
             name:'吴荣华',
-            points:'6',
-            head:'1'
+            points:'11',
+            head:'3'
         },
         {
             name:'易晓健',
-            points:'6',
-            head:'0'
+            points:'8',
+            head:'1'
         },
         {
             name:'陈欣欣',
-            points:'5',
+            points:'9',
             head:'5'
         },
         {
             name:'关智梁',
-            points:'2',
+            points:'4',
             head:'1'
         },
         {
             name:'柯晓凯',
-            points:'5',
-            head:'3'
+            points:'8',
+            head:'6'
         },
         {
             name:'谢玄亮',
-            points:'2',
-            head:'0'
+            points:'4',
+            head:'2'
         },
         {
             name:'谢瑛',
-            points:'2',
-            head:'0'
+            points:'9',
+            head:'3'
         },
         {
             name:'曾帆',
-            points:'0',
+            points:'4',
             head:'0'
         },
         {
             name:'何炜华',
-            points:'4',
-            head:'0'
-        },
-        {
-            name:'刘威龙',
-            points:'5',
+            points:'10',
             head:'2'
         },
         {
+            name:'刘威龙',
+            points:'10',
+            head:'4'
+        },
+        {
             name:'周子文',
-            points:'4',
+            points:'6',
             head:'3'
         },
         {
             name:'陈育文',
-            points:'6',
-            head:'1'
+            points:'13',
+            head:'3'
         },
         {
             name:'胡慧贤',
@@ -83,8 +83,8 @@ module.exports = {
         },
         {
             name:'吴诗伟',
-            points:'8',
-            head:'0'
+            points:'16',
+            head:'3'
         }
 
     ]
